e9ee8d49dc6f43360107204859e2e7f0bbf9bafa
mostafamedhat1983 <mostafamomo@gmail.com>
2026-06-21 23:18:12 +0300

    adding validation to vpc cidr variable

--- terraform/modules/Networking/variables.tf
+++ terraform/modules/Networking/variables.tf
@@ -1,6 +1,12 @@
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
+
+  # Validates CIDR format by attempting to calculate the first IP; can() catches errors if invalid
+  validation {
+    condition     = can(cidrhost(var.vpc_cidr_block, 0))
+    error_message = "Must be a valid CIDR block (e.g. 10.0.0.0/16)."
+  }
 }
 
 variable "vpc_name" {
